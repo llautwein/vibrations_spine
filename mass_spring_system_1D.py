4d8e9151eb05b73d7c2e48c81ef3566911003503
Lucas Lautwein <s4lulaut@uni-trier.de>
2024-11-30 21:44:39 +0000

    Initial commit

---
+++ mass_spring_system_1D.py
@@ -0,0 +1,44 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+
+def system_of_equations(t, y, A, B):
+    dydt = A @ y + B * np.sin(t)
+    return dydt
+
+
+mu = 1
+alpha = 1
+
+A = np.array([
+    [0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 1],
+    [-2, 1, 0, -2*mu, mu, 0],
+    [1, -2, 1, mu, -2*mu, mu],
+    [0, 1, -1, 0, mu, -mu]
+])
+
+B = np.array([0, 0, 0, alpha, 0, 0])
+
+y0 = np.zeros(6)
+#y0 = np.array([1, 0, 0, -1, 0, 0])
+t_span = (0, 50)
+t_eval = np.linspace(t_span[0], t_span[1], 10000)
+
+sol = solve_ivp(system_of_equations, t_span, y0, args=(A, B), method="RK45", atol=1e-6, rtol=1e-6, t_eval=t_eval)
+
+plt.figure(figsize=(10, 6))
+
+for i in range(3):
+    plt.plot(sol.t, sol.y[i], label=f"Displacement of mass {i+1}")
+
+# plt.plot(sol.t, np.sin(sol.t), label=f"Sinusoidal vibration")
+
+plt.title('Displacement of each mass in the system')
+plt.xlabel('Time')
+plt.ylabel('Displacement')
+plt.legend()
+plt.grid(True)
+plt.show()
